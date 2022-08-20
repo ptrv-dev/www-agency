8656162c840b457925d61fab92b8fb1e4fe22fb0
ptrv-dev <petrov99119@gmail.com>
2022-08-20 21:58:53 +0300

    Update 2022-08-20T18:58:50.204Z

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-const burgerButton = document.querySelector('button.header__burger');
-const nav = document.querySelector('nav.nav');
-
-burgerButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    burgerButton.classList.toggle('header__burger_active');
-    nav.classList.toggle('nav_active');
-    document.body.classList.toggle('no-scroll');
-})
